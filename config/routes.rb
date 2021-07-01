e545cb64e4edc5d4733e3952983733fb902e4304
Xuzhe Xu <xuxuzhe@163.com>
2021-07-01 20:40:33 +0800

    some updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :decks, only: [:show] do
-    resources :cards, only: [:show, :update] do
-      resources :user_cards, only: [:update]
-    end
+  resources :decks do
+    resources :cards, only: [:new, :create]
   end
 
+  resources :cards, except: [:new, :create]
+
+  resources :user_cards, only: [:update]
 
 end
