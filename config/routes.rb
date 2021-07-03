73530b4637885321bcdded3aae04da1bb6c5f7e1
Xuzhe Xu <xuxuzhe@163.com>
2021-07-03 13:54:58 +0800

    deck_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :decks do
-    resources :cards, only: [:index, :new, :create]
+    resources :cards, only: [:new, :create]
   end
 
   resources :cards, except: [:new, :create]
 
   resources :user_cards, only: [:update]
+
+  resources :user_decks, only: [:update]
 
 end
