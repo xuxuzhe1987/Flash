a427ca5db593529be918af067afe2aeaf107b329
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-07-03 14:37:34 +0800

    added the actions on the card controller - deleted Show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,50 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:edit, :update, :destroy]
+  before_action :set_deck, only: [:new, :create]
 
+  def index
+    @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.deck = @deck
+    @card.save
+    # if @card.save
+    redirect_to new_deck_card_path(@deck)
+    # else
+    #   render.new
+    # end
+  end
+
+  def edit
+  end
+
+  def update
+    @card.update(card_params)
+    redirect_to deck_path(@card.deck_id)
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to deck_path(@card.deck_id)
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:question, :answer, :hint)
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def set_deck
+    @deck = Deck.find(params[:deck_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :decks do
-    resources :cards, only: [:index, :new, :create]
+    resources :cards, only: [:new, :create]
   end
 
-  resources :cards, except: [:new, :create]
+  resources :cards, except: [:new, :create, :show]
 
   resources :user_cards, only: [:update]
 
