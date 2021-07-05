dd8853bfac6e0fa549fff1aabee2d82357f35ec1
Aaron Golec <aaron.sescie@gmail.com>
2021-07-05 16:03:40 +0800

    pundit installed, with no authorization put in place everything is open

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,36 +1,42 @@
 class DecksController < ApplicationController
   def index
-    @decks = Deck.all
+    @decks = policy_scope(Deck)
   end
 
   def show
     @deck = Deck.find(params[:id])
+    authorize @deck
   end
 
   def new
     @deck = Deck.new
+    authorize @deck
   end
 
   def create
     @deck = Deck.new(deck_params)
     @deck.save
     redirect_to new_deck_card_path(@deck)
+    authorize @deck
   end
 
   def edit
     @deck = Deck.find(params[:id])
+    authorize @deck
   end
 
   def update
     @deck = Deck.find(params[:id])
     @deck.update(deck_params)
     redirect_to deck_path(@deck)
+    authorize @deck
   end
 
   def destroy
     @deck = Deck.find(params[:id])
     @deck.destroy
     redirect_to decks_path
+    authorize @deck
   end
 
   private
