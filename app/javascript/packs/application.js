c5d475fc3c2ea2b48038c31bf5f4dc5c2436ee97
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-07-08 21:53:50 +0800

    first script added, need to refactor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,4 +31,11 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  // flashCard();
+  // flashView();
+  // nextCard();
+  // slideIn();
+  // slideOut();
 });
+
+
