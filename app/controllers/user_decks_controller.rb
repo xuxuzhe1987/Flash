29f9bcd2d2141a7225ad64eeabb3d2ed38891924
Xuzhe Xu <xuxuzhe@163.com>
2021-07-09 15:35:09 +0800

    user_deck_controller

--- app/controllers/user_decks_controller.rb
+++ app/controllers/user_decks_controller.rb
@@ -1,2 +1,19 @@
 class UserDecksController < ApplicationController
+  def create
+    @user_deck = UserDeck.new(user_deck_params)
+    authorize @user_deck
+    @user_deck.save
+    redirect_to user_deck_path(@user_deck)
+  end
+
+  def show
+    @user_deck = UserDeck.find(params[:id])
+    authorize @user_deck
+  end
+
+  private
+
+  def user_deck_params
+    params.permit(:status, :deck_id, :user_id)
+  end
 end
