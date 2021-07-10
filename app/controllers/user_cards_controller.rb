8504a5571911b876d2b659d8f916384796422a53
Aaron Golec <aaron.sescie@gmail.com>
2021-07-10 10:15:36 +0800

    just some extra policy stuff for the user cards

--- app/controllers/user_cards_controller.rb
+++ app/controllers/user_cards_controller.rb
@@ -1,22 +1,7 @@
 class UserCardsController < ApplicationController
 
-
-  # # create_table "user_cards", force: :cascade do |t|
-  # #   t.boolean "result"
-  # #   t.text "answer"
-  # #   t.bigint "card_id", null: false
-  # #   t.datetime "created_at", precision: 6, null: false
-  # #   t.datetime "updated_at", precision: 6, null: false
-  # #   t.index ["card_id"], name: "index_user_cards_on_card_id"
-  # # end
-  # #
-  # # the action will be done in the view (knew: true, didn't know: false, by default everything is false)
-  # # find the card in the deck
-  # # assign the card with the result (known) true or (unknown) false
-  # # it will be pushed into an array
-  # # it will then be sorted
-  # # it will then by handled by the user decks to display
-  # # it is a post, no it should be a form I think
-  # def result
-  # end
 end
+
+
+
+
